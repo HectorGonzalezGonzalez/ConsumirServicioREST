bb2dcb18d90a64a7e2eb8c7112d6e36b344dd4b7
HectorGonzalezGonzalez <37567557+HectorGonzalezGonzalez@users.noreply.github.com>
2020-06-23 13:03:06 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace ConsumirServicioREST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient clientHttp = new HttpClient();
+            clientHttp.BaseAddress = new Uri("http://www.misistemaweb.com.mx/");
+            var request = clientHttp.GetAsync("API/APIUsuarios").Result;
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var listado = JsonConvert.DeserializeObject<List<Usuario>>(resultString);
+                foreach (Usuario item in listado)
+                {
+                    Console.WriteLine(item.Nombre);
+                }
+                
+            }            
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsumirServicioREST
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Paterno { get; set; }
+        public string Materno { get; set; }
+        public string Email { get; set; }
+        public string NombreDeUsuario { get; set; }
+        public string Password { get; set; }
+        public bool Estatus { get; set; }
+        public DateTime Fecha_creo { get; set; }
+        public string UsrCreoId { get; set; }
+    }
+}
